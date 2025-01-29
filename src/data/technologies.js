ddc2783abf992695f0043473bd1f3c5fce926c0f
Gustavo Yoshizawa <gustavoyoshizawa89@gmail.com>
2025-01-28 21:52:27 -0300

    ajustes imagens

--- src/data/technologies.js
+++ src/data/technologies.js
@@ -1,25 +1,25 @@
 const technologies = [
-  { name: "HTML", src: "/src/assets/icons/html.png", alt: "HTML icon" },
-  { name: "CSS", src: "/src/assets/icons/css.png", alt: "CSS icon" },
+  { name: "HTML", src: "/assets/icons/html.png", alt: "HTML icon" },
+  { name: "CSS", src: "/assets/icons/css.png", alt: "CSS icon" },
   {
     name: "Javascript",
-    src: "/src/assets/icons/js.png",
+    src: "/assets/icons/js.png",
     alt: "Javascript icon",
   },
   {
     name: "Bootstrap",
-    src: "/src/assets/icons/bootstrap.png",
+    src: "/assets/icons/bootstrap.png",
     alt: "Bootstrap icon",
   },
-  { name: "React", src: "/src/assets/icons/react.png", alt: "React icon" },
-  { name: "SASS", src: "/src/assets/icons/sass.png", alt: "SASS icon" },
+  { name: "React", src: "/assets/icons/react.png", alt: "React icon" },
+  { name: "SASS", src: "/assets/icons/sass.png", alt: "SASS icon" },
   {
     name: "WordPress",
-    src: "/src/assets/icons/wordpress.png",
+    src: "/assets/icons/wordpress.png",
     alt: "WordPress icon",
   },
-  { name: "PHP", src: "/src/assets/icons/php.png", alt: "PHP icon" },
-  { name: "Figma", src: "/src/assets/icons/Figma.png", alt: "Figma icon" },
+  { name: "PHP", src: "/assets/icons/php.png", alt: "PHP icon" },
+  { name: "Figma", src: "/assets/icons/Figma.png", alt: "Figma icon" },
 ];
 
 export default technologies;
